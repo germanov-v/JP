6c15fb96193bfac0e283849136aee536d8b43a13
valentin <germanov.ve@gmail.com>
2026-02-11 11:09:59 +0300

    fix web.xml

--- blog-dashboard-v1/src/main/java/ru/blog/configuration/WebConfiguration.java
+++ blog-dashboard-v1/src/main/java/ru/blog/configuration/WebConfiguration.java
@@ -6,6 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("ru.blog")
+@ComponentScan(basePackages = "ru.blog")
 public class WebConfiguration {
 }

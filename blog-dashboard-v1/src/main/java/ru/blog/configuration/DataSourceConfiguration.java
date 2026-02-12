866a9056a0f903a8bf4dc82c0bdc7365458bf504
valentin <germanov.ve@gmail.com>
2026-02-12 09:28:45 +0300

    upd db

--- blog-dashboard-v1/src/main/java/ru/blog/configuration/DataSourceConfiguration.java
+++ blog-dashboard-v1/src/main/java/ru/blog/configuration/DataSourceConfiguration.java
@@ -1,9 +1,15 @@
 package ru.blog.configuration;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -14,18 +20,52 @@ public class DataSourceConfiguration {
     public DataSource dataSource(
             @Value("${spring.datasource.url}") String url,
             @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password
+            @Value("${spring.datasource.password}") String password,
+
+            @Value("${spring.datasource.hikari.maximumPoolSize:10}") int maximumPoolSize,
+            @Value("${spring.datasource.hikari.minimumIdle:2}") int minimumIdle,
+            @Value("${spring.datasource.hikari.connectionTimeout:30000}") long connectionTimeout,
+            @Value("${spring.datasource.hikari.idleTimeout:600000}") long idleTimeout,
+            @Value("${spring.datasource.hikari.maxLifetime:1800000}") long maxLifetime
 
             )
     {
 
-        var dataSource = new DriverManagerDataSource();
+//        var dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl(url);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(url);
-        dataSource.setUsername(url);
-        dataSource.setPassword(url);
+
+        dataSource.setMaximumPoolSize(maximumPoolSize);
+        dataSource.setMinimumIdle(minimumIdle);
+        dataSource.setConnectionTimeout(connectionTimeout);
+        dataSource.setIdleTimeout(idleTimeout);
+        dataSource.setMaxLifetime(maxLifetime);
+
         return dataSource;
     }
 
-    // TODO: add @EventListener
+
+    @EventListener
+    public void dataSeed(ContextRefreshedEvent event) {
+       var dataSource = event.getApplicationContext().getBean(DataSource.class);
+
+       var seeder = new ResourceDatabasePopulator();
+       seeder.setSqlScriptEncoding("UTF-8");
+       seeder.addScript(new ClassPathResource("migrations/init_migration.sql"));
+       seeder.execute(dataSource);
+    }
+
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }

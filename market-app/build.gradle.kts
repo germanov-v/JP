9a98b1e894c889966a88fa88d5c8af6a925d3141
valentin <germanov.ve@gmail.com>
2026-03-01 13:15:43 +0300

    initial project - clean gradle

--- market-app/build.gradle.kts
+++ market-app/build.gradle.kts
@@ -1,37 +1,19 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.5.11"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("java")
 }
 
-group = "ru.yp"
-version = "0.0.1-SNAPSHOT"
-description = "market-app"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+group = "ru.yp.marketapp"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.liquibase:liquibase-core")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    runtimeOnly("org.postgresql:postgresql")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
 }

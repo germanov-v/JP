da50be8e73edd22feacc94c6b226052c791a80ed
valentin <germanov.ve@gmail.com>
2026-03-11 15:57:07 +0300

    upd CartReactiveRepository

--- market-app-react/adapters/persistence/src/main/java/ru/yp/marketapp/adapters/persistence/r2dbc/repo/CartReactiveRepository.java
+++ market-app-react/adapters/persistence/src/main/java/ru/yp/marketapp/adapters/persistence/r2dbc/repo/CartReactiveRepository.java
@@ -1,9 +1,27 @@
 package ru.yp.marketapp.adapters.persistence.r2dbc.repo;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.yp.marketapp.adapters.persistence.entity.CartEntity;
 
 public interface CartReactiveRepository extends ReactiveCrudRepository<CartEntity, Long> {
     Mono<CartEntity> findFirstByOrderByIdAsc();
+
+
+
+    @Query("""
+        select id
+        from market.cart
+        order by id desc
+        limit :limit offset :offset
+        """)
+    Flux<Long> findPageIds(int offset, int limit);
+
+    @Query("""
+        select count(*)
+        from market.cart
+        """)
+    Mono<Long> countAll();
 }

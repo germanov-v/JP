7b4b1997802fb1d069c26f5c347f000673b5d80e
valentin <germanov.ve@gmail.com>
2026-03-11 14:22:26 +0300

    upd reactive repositories

---
+++ market-app-react/adapters/persistence/src/main/java/ru/yp/marketapp/adapters/persistence/repository/OrderRepositoryAdapter.java
@@ -0,0 +1,21 @@
+package ru.yp.marketapp.adapters.persistence.repository;
+
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.yp.marketapp.appplication.repositories.OrderRepository;
+import ru.yp.marketapp.domain.order.Order;
+
+
+@Repository
+public class OrderRepositoryAdapter implements OrderRepository {
+    @Override
+    public Flux<Order> findAll() {
+        return null;
+    }
+
+    @Override
+    public Mono<Order> findById(long id) {
+        return null;
+    }
+}
